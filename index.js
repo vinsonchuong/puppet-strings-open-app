ecfab9fb89aaec2e57bac7e9b04983bbb7371a41
Vinson Chuong <vinsonchuong@gmail.com>
2019-04-25 14:01:27 -0700

    fix(openApp): Deal with regenerator dependency

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 /* @flow */
 import type { Tab } from 'puppet-strings'
 import { openChrome, openTab, closeBrowser } from 'puppet-strings'
-import Bundler from 'parcel-bundler'
-import tempy from 'tempy'
-import getPort from 'get-port'
+import {
+  getPort,
+  startServer,
+  stopServer,
+  respondToRequests,
+  serveUi
+} from 'passing-notes'
 
 export default async function(appPath: string): Promise<Tab> {
-  const bundler = new Bundler(appPath, {
-    watch: false,
-    outDir: tempy.directory(),
-    logLevel: 1
-  })
   const port = await getPort()
+  const server = await startServer(
+    port,
+    respondToRequests(serveUi({ entry: appPath, log: () => () => {} }))
+  )
 
-  const [server, browser] = await Promise.all([
-    bundler.serve(port),
-    openChrome()
-  ])
-
+  const browser = await openChrome()
   const tab = await openTab(browser, `http://localhost:${port}`)
 
   // Always true--to appease the type-checker
   if (tab.puppeteer) {
     tab.puppeteer.page.close = async () => {
       await closeBrowser(browser)
-      server.close()
+      await stopServer(server)
     }
   }
 
